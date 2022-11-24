e869eadaa0646d6e5f184821eddfbb1c0a822aad
Lachgar <lachgar.m@gmail.com>
2022-11-24 08:49:03 +0100

    findByMachines

--- src/services/MachineService.java
+++ src/services/MachineService.java
@@ -100,5 +100,21 @@ public class MachineService implements IDao<Machine> {
 		}
 		return machines;
 	}
+	
+	
+	public List<Machine> findMachinesBySalle(Salle s) {
+		List<Machine> machines = new ArrayList<Machine>();
+		try {
+			String sql = "select * from machine where salle = "+ s.getId();
+			Statement st = Connexion.getConnection().createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next())
+				machines.add(new Machine(rs.getInt("id"), rs.getString("reference"), rs.getString("marque"),
+						rs.getDouble("prix"), rs.getDate("dateAchat"), ss.findById(rs.getInt("salle"))));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return machines;
+	}
 
 }
